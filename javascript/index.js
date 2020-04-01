65b171386244fc79456aa7fdfe33074cafae0568
kmpit <kimmcmoo@gmail.com>
2020-04-01 19:00:03 -0400

    started sidebar

--- javascript/index.js
+++ javascript/index.js
@@ -31,3 +31,49 @@ function getCurrentTerm(){
 
     document.getElementById('current_term').innerHTML = (term+' Systems Design Engineering');
 }
+
+//event listener to close the sidebar
+document.addEventListener("click", function(event) {
+    console.log('event listern fired');
+    // check that sidebar is opened
+    var width = parseInt(document.getElementById("sidebar").style.width);
+    console.log('wdith is', width);
+    if (width > 0 ){
+        console.log('got into the cmd');
+        // If user clicks inside the sidebar, do nothing
+        if (event.target.closest(".sidebar")) {
+            return;
+        }
+        // If user is just opening the sidebar
+        else if (event.target.closest(".button")) {
+            return;
+        }
+        // else close the sidebar
+        else {
+            closeMenu(); 
+        }
+     }
+});
+
+function sidebarOnClick(){
+    var sidebar_width = parseInt(document.getElementById("sidebar").style.width);
+    if (sidebar_width > 0 ){
+        closeMenu();
+        return;
+    }
+    else {
+        openMenu();
+    }
+
+}
+
+// functions to close and open the sidebar
+function openMenu(){
+    document.getElementById("sidebar").style.width = "250px";
+    console.log('width is ', document.getElementById("sidebar").style.width);
+}
+
+function closeMenu(){
+    console.log('close menu called');
+    document.getElementById("sidebar").style.width = "0px";
+}
